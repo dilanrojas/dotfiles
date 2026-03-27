b576037a54cd07db798a1bf877d573ebffe1ec6e
Dilan Rojas <rojasvargasdilan010@gmail.com>
2026-03-26 22:45:20 -0600

    feat: fonts + workspaces

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -12,7 +12,7 @@ alias update "sudo pacman -Syu && yay -Syu"
 alias n nvim
 
 alias cputemp "sensors | grep CPU"
-alias rich "rich --pager --theme solarized-dark --padding 3,10,3,10"
+alias rich "rich --pager --theme one-dark --padding 3,10,3,10"
 
 alias i "sudo pacman -S --noconfirm"
 alias s "pacman -Ss"
@@ -21,10 +21,22 @@ alias yi "yay -S --noconfirm"
 alias ys "yay -Ss"
 alias yr "yay -Rns"
 
+# Systemd
+alias start "sudo systemctl start"
+alias startu "systemctl --user start"
+alias enable "sudo systemctl enable"
+alias enableu "systemctl --user enable"
+alias stop "sudo systemctl stop"
+alias stopu "systemctl --user stop"
+alias disable "sudo systemctl disable"
+alias disableu "systemctl --user disable"
+alias sstatus "systemctl status"
+alias sstatussu "systemctl --user status"
+
 alias mount "udisksctl mount -b"
 alias umount "udisksctl unmount -b"
 
-alias typingtest "tt -t 30 -theme solarized-dark"
+alias typingtest "tt -t 30 -theme one-dark"
 
 # Prompt
 starship init fish | source
