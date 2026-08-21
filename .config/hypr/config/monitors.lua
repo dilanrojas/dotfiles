d30cf7fb365fbc1bcdc54e308683d23f41e627f4
Dilan Rojas <rojasvargasdilan010@gmail.com>
2026-08-21 13:26:28 -0600

    feat: assign workspaces to monitors

--- .config/hypr/config/monitors.lua
+++ .config/hypr/config/monitors.lua
@@ -17,15 +17,22 @@ hl.monitor({
 
 	-- position = "1920x0", -- Right
 	-- position = "-1920x0", -- Left
-	-- position = "0x-1200", -- Top
+	position = "0x-1200", -- Top
 	-- position = "0x1200",  -- Bottom
 
 	-- mirror = "eDP-1",
-
 	scale = "1",
 })
 
--- Assign workspaces 6-10 to HDMI-A-1
+-- Assign workspaces
+for i = 1, 5 do
+	hl.workspace_rule({
+		workspace = tostring(i),
+		monitor = "eDP-1",
+		default = (i == 1),
+	})
+end
+
 for i = 6, 10 do
 	hl.workspace_rule({
 		workspace = tostring(i),
